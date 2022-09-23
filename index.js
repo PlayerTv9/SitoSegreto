8d6139e438bd1bcac26624032c8f329a5091734e
PlayerTv9 <tommaso.ibba09@gmail.com>
2022-09-23 16:01:15 +0200

    1.0

---
+++ index.js
@@ -0,0 +1,21 @@
+/*function Berlusconi(){
+	window.open("https://www.youtube.com/watch?v=iZ5xzkU7HGg", "_blank")
+}*/
+
+function ciao() {
+	alert("Ti avevo avvisato...");
+	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
+
+}
+
+function segretissima() {
+	alert("OK stavolta mi hai stupito...");
+	window.open("https://youtu.be/blLJ5UIu3r8", "_blank")
+
+}
+
+function supersegretissima() {
+	alert("Stai per vedere le mie carte di credito...");
+	window.open("https://www.youtube.com/watch?v=RvY5ploo1OI", "_blank")
+
+}
